8e0a74e0a5e6d702385f12cbfaa2e3c93e5f1ff1
Manan Bhatt <93242634+Manan2651@users.noreply.github.com>
2023-09-23 20:21:23 +0530

    Add files via upload

---
+++ Topper_World/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const learnMoreBtn = document.getElementById('learn-more-btn');
+    learnMoreBtn.addEventListener('click', function() {
+        // Handle button click action here
+        // For example, scroll to a specific section or show a modal
+    });
+});
